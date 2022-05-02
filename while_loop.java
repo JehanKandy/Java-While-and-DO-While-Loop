7b45d8394eea7e784bba11a98d1cd0a662e25ad6
JehanKandy <89431184+JehanKandy@users.noreply.github.com>
2022-05-02 21:22:53 +0530

    Add files via upload

---
+++ while_loop.java
@@ -0,0 +1,21 @@
+public class while_loop {
+    public static void main(String[] args) {
+        int i = 0;
+
+        //While loop
+        while(i < 5){
+            System.out.println("Hello");
+            i++;
+        }
+
+        //do while loop
+
+        int k = 0;
+        do{
+            System.out.println("hello Do while loop");
+            k++;
+        }
+        while(k < 5);
+    }
+    
+}
